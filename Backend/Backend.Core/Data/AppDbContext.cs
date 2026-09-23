f5962c0ca8b970fec8e6a4d0b993a1c2b4514c7d
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2026-09-23 12:43:21 +0800

    Configure product transaction batch delete behavior

--- Backend/Backend.Core/Data/AppDbContext.cs
+++ Backend/Backend.Core/Data/AppDbContext.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Backend.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Core.Data;
 
@@ -10,4 +10,20 @@ public class AppDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductBatch> ProductBatches { get; set; }
     public DbSet<ProductTransaction> ProductTransactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        /*
+        By default, deleting a batch also deletes every transaction tied to it.
+        This is a result of DeleteBehavior.Cascade. So, we enforce a rule where
+        when a batch is deleted, the product transactions tied to it will not be
+        deleted. Instead, their batch property will simply be set to null.
+        */
+        builder.Entity<ProductTransaction>()
+            .HasOne(t => t.Batch)
+            .WithMany()
+            .OnDelete(DeleteBehavior.SetNull);
+    }
 }
