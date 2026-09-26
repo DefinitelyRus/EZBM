dca3e53803572fe2cbe032a58dc25ce6a4ed822d
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2026-09-27 00:27:38 +0800

    Add database migration for updated models

---
+++ Backend/Backend.Core/Migrations/20260926162407_UpdateModels.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockLevel",
+                table: "Products",
+                newName: "QuantityType");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Barcode",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsForSale",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongDescription",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescription",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sku",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalQuantity",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionMethod",
+                table: "Products",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "ProductBatches",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Cost = table.Column<decimal>(type: "TEXT", nullable: false),
+                    BatchNumber = table.Column<long>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductBatches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductBatches_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductId = table.Column<long>(type: "INTEGER", nullable: false),
+                    BatchId = table.Column<long>(type: "INTEGER", nullable: true),
+                    OverrideMethod = table.Column<int>(type: "INTEGER", nullable: true),
+                    Quantity = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TransactionType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductTransactions_ProductBatches_BatchId",
+                        column: x => x.BatchId,
+                        principalTable: "ProductBatches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_ProductTransactions_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBatches_ProductId",
+                table: "ProductBatches",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTransactions_BatchId",
+                table: "ProductTransactions",
+                column: "BatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTransactions_ProductId",
+                table: "ProductTransactions",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTransactions");
+
+            migrationBuilder.DropTable(
+                name: "ProductBatches");
+
+            migrationBuilder.DropColumn(
+                name: "Barcode",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsForSale",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LongDescription",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ShortDescription",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sku",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TotalQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionMethod",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "QuantityType",
+                table: "Products",
+                newName: "StockLevel");
+        }
+    }
+}
